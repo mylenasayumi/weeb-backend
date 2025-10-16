b4c764f532e8b71ad15ba8b9ab3e3b93b8e4271b
mylenasayumi <mylena.sayumi@gmail.com>
2025-10-17 01:39:13 +0200

    fix(settings): adding INSTALLED_APPS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # tiers
     'corsheaders',
     'rest_framework',
+    'django_filters',
+    # apps
+    'users',
     'articles',
+    'satisfactions',
 ]
 
 MIDDLEWARE = [
