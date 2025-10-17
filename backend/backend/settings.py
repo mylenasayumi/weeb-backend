d9efe2290df675487be4e2a69fce41d660aa32b0
chloé-océane harb <146004756+Chloxhrb@users.noreply.github.com>
2025-10-17 14:24:36 +0200

    Remove CORS middleware comment from settings
    Removed commented recommendation for CORS middleware placement.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,11 +52,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # (Optionnel mais recommandé : placer CORS tôt)
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
