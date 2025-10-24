e16cd07ae71d5e7b7619540d5c27085266f2e339
mylenasayumi <mylena.sayumi@gmail.com>
2025-10-24 15:00:35 +0200

    fix(satisfaction): change from ModelViewSet to CreateAPIView

--- backend/satisfactions/views.py
+++ backend/satisfactions/views.py
@@ -1,27 +1,18 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework import viewsets
+from rest_framework.generics import CreateAPIView
 
 from .models import Satisfaction
 from .serializers import SatisfactionSerializer
 
-class SatisfactionViewSet(viewsets.ModelViewSet):
+class SatisfactionViewSet(CreateAPIView):
     """
-    ViewSet to save a satisfaction comment in the database.
+    View to save a satisfaction comment in the database.
     Handle CRUD operations on Satisfactions : list / retrieve / create / update / partial_update / destroy
     """
 
     queryset = Satisfaction.objects.all()
     serializer_class = SatisfactionSerializer
-
-    def post(self, request):
-        serializer = SatisfactionSerializer(data=request.data)
-
-        if serializer.is_valid():
-            # Save in the database
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def perform_create(self, serializer):
         """
