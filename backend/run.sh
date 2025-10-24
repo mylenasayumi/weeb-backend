8db72590363fb03ab8c137b0eb4c285c3b39bd26
PierreSaumet <psaumet.dvp@gmail.com>
2025-10-24 11:50:51 +0200

    feat(fixtures): adding 5 users and 10 articles in the database when starting project

--- backend/run.sh
+++ backend/run.sh
@@ -6,5 +6,10 @@ python manage.py makemigrations
 # Apply migrations
 python manage.py migrate
 
+# Add fixtures
+python manage.py loaddata users/fixtures/users_fixtures.json || true
+python manage.py loaddata articles/fixtures/articles_fixtures.json || true
+
+
 # Start server
 python manage.py runserver 0.0.0.0:8000
