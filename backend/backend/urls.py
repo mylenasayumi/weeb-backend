9b560b7e50e7bfc58c518b504445c6136cc62478
mylenasayumi <mylena.sayumi@gmail.com>
2025-10-24 12:29:59 +0200

    fix(urls): adding satisfations to router in urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,10 +21,12 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.views import UserViewSet
+from satisfactions.views import SatisfactionAPIView
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="users")
 router.register(r"articles", ArticleViewSet, basename="articles")
+router.register(r"satisfactions", SatisfactionAPIView, basename="satisfactions")
 
 urlpatterns = [
     # Admin
