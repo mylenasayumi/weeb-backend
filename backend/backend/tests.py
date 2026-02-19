062f23d4a5231744e6ee0df6a91ad171cbc7fd65
mylenasayumi <mylena.sayumi@gmail.com>
2026-02-19 16:50:59 +0100

    Correcting tests

--- backend/backend/tests.py
+++ backend/backend/tests.py
@@ -9,7 +9,9 @@ from django.test import SimpleTestCase
 class SettingsCITest(unittest.TestCase):
     def test_ci_database_settings(self):
         with mock.patch.dict("os.environ", {"CI": "true"}):
-            settings = importlib.reload(importlib.import_module("backend.settings"))
+            settings = importlib.reload(
+                importlib.import_module("backend.settings.development")
+            )
             self.assertEqual(
                 settings.DATABASES["default"]["ENGINE"], "django.db.backends.sqlite3"
             )
