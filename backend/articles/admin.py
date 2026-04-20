b79e83629abee363e17c1b3aea0b0ab77d2baf83
mylenasayumi <mylena.sayumi@gmail.com>
2026-04-20 20:44:01 +0200

    Improving the admin's page

--- backend/articles/admin.py
+++ backend/articles/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from .models import Article
 
-admin.site.register(Article)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "user", "views", "created_at")
+    search_fields = (
+        "title",
+        "description",
+        "user__email",
+        "user__first_name",
+        "user__last_name",
+    )
+    list_filter = ("created_at",)
+    ordering = ("-created_at",)
