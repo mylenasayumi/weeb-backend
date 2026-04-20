1558e123e30f98f5b5ad98fae5da4d663166f995
PierreSaumet <psaumet.dvp@gmail.com>
2026-04-20 18:05:29 +0200

    feat(charts): update cache

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -70,7 +70,7 @@ TEMPLATES = [
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://weeb-redis:6379",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
