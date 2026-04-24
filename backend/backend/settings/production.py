bc2fbae76cfa0edc35c8988cb639f81de25d35c0
PierreSaumet <psaumet.dvp@gmail.com>
2026-04-24 12:54:09 +0200

    add corls allow credentials

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -12,6 +12,7 @@ DEBUG = os.getenv("DEBUG", False).lower() == "true"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
 
@@ -32,9 +33,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Frontend URLs : should be set to the actual frontend URLs in production
 # ==============================================================================
 
-ALLOWED_FRONTEND_URLS = [
-    "http://localhost:5173",  # Vite dev server
-]
 
 SESSION_COOKIE_SAMESITE = "None"
 CSRF_COOKIE_SAMESITE = "None"
